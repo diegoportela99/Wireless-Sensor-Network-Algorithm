b35fa44f672cdea72438a25f2acba98b77ca0288
diegoportela99 <diegofportela1@gmail.com>
2021-05-07 16:34:58 +1000

    path finder

--- Algorithm_testing.py
+++ Algorithm_testing.py
@@ -3,27 +3,52 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as anim
 import random
 
+#Variables
+graphsToTest = 50
+numberOfNodes = 10
+weightMax = 10
+
+#default as node 0
+sourceNode = 0
+
+#best path map
+mapPath = []
+
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
-for x in range(1,50):
-    G = nx.barabasi_albert_graph(10, 2)
+#in range of nodes, select one at random for target node.
+sourceNode = (random.randint(1, numberOfNodes))
+
+color_map = []
+
+#Generate random graph structures to represent different scenarios
+for x in range(1,graphsToTest):
+    G = nx.barabasi_albert_graph(numberOfNodes, 2)
 
     for (u,v,w) in G.edges(data=True):
-        w['weight'] = random.randint(1,10)
+        w['weight'] = random.randint(1,weightMax)
+        color_map.append('blue')
 
     pos=nx.spring_layout(G)
     nx.draw_networkx(G,pos)
+    nx.draw_networkx_nodes(G, pos, node_size=1, node_color='red')
     labels = nx.get_edge_attributes(G,'weight')
     nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
     
     fig.canvas.draw()
-    print(list(G.nodes))
-    print(list(G.edges(data=True)))
+    #print(list(G.nodes))
+    #print(list(G.edges(data=True)))
+
     
-    plt.pause(0.5)
+    print("source node is: " + str(numberOfNodes-1))
+    print("sink node is: 0")
+    #for each node, find best route to sink
+    print(nx.shortest_path(G, source=numberOfNodes-1, target=0))
+ 
+    plt.pause(10)
     fig.clear()
     G.clear()
     
-    
+
