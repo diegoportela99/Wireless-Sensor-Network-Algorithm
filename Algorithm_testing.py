f0f5b68feec1a7afcbe1eeb171db3092bc134d16
diego <diegofportela1@gmail.com>
2021-05-13 16:43:37 +1000

    updated graph / coloring

--- Algorithm_testing.py
+++ Algorithm_testing.py
@@ -4,9 +4,8 @@ import matplotlib.animation as anim
 import random
 
 #Variables
-graphsToTest = 50
-numberOfNodes = 10
-weightMax = 10
+numberOfNodes = 30
+weightMax = 15
 
 #default as node 0
 sourceNode = 0
@@ -17,38 +16,39 @@ mapPath = []
 plt.ion()
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax2 = fig.add_subplot(111)
 
 #in range of nodes, select one at random for target node.
 sourceNode = (random.randint(1, numberOfNodes))
 
 color_map = []
 
-#Generate random graph structures to represent different scenarios
-for x in range(1,graphsToTest):
-    G = nx.barabasi_albert_graph(numberOfNodes, 2)
+#Generate random graph structure to represent different scenario
+G = nx.barabasi_albert_graph(numberOfNodes, 1)
 
-    for (u,v,w) in G.edges(data=True):
-        w['weight'] = random.randint(1,weightMax)
-        color_map.append('blue')
+for (u,v,w) in G.edges(data=True):
+    w['weight'] = random.randint(1,weightMax)
 
-    pos=nx.spring_layout(G)
-    nx.draw_networkx(G,pos)
-    nx.draw_networkx_nodes(G, pos, node_size=1, node_color='red')
-    labels = nx.get_edge_attributes(G,'weight')
-    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+mapPath = nx.shortest_path(G, source=numberOfNodes-1, target=0)
+ax2.set_title(label=mapPath, fontsize=20, color="red")
     
-    fig.canvas.draw()
-    #print(list(G.nodes))
-    #print(list(G.edges(data=True)))
+for node in G:
+    if node in mapPath:
+        color_map.append('red')
+    else: 
+        color_map.append('black') 
 
     
-    print("source node is: " + str(numberOfNodes-1))
-    print("sink node is: 0")
-    #for each node, find best route to sink
-    print(nx.shortest_path(G, source=numberOfNodes-1, target=0))
- 
-    plt.pause(10)
-    fig.clear()
-    G.clear()
+pos=nx.spring_layout(G)
+nx.draw(G, pos, node_size=500, node_color=color_map,  with_labels=True)
+
+labels = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+    
+fig.canvas.draw()
+
+print("source node is: " + str(numberOfNodes-1))
+print("sink node is: 0")
+print("Optimal Route " + str(mapPath))
     
 
